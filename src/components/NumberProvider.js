5865d63e223ce1a2f1d4f0683ef3e165487c4e53
riho <rihoriho1031@gmail.com>
2020-07-20 18:15:30 +0900

    first commit

---
+++ src/components/NumberProvider.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+export const NumberContext = React.createContext();
+
+const NumberProvider = (props) => {
+  const number = "0";
+  return (
+    <NumberContext.Provider value={{ number }}>
+      {props.children}
+    </NumberContext.Provider>
+  );
+};
+
+export default NumberProvider;
